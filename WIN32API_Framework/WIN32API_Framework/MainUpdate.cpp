5c2d98fad45720c3ca76967da2880a30431294d4
zune710 <zune710@gmail.com>
2023-05-03 09:18:45 +0900

    0503

--- WIN32API_Framework/WIN32API_Framework/MainUpdate.cpp
+++ WIN32API_Framework/WIN32API_Framework/MainUpdate.cpp
@@ -1,7 +1,7 @@
 #include "MainUpdate.h"
 #include "Player.h"
 
-MainUpdate::MainUpdate() : m_pPlayer(NULL)
+MainUpdate::MainUpdate() : m_hdc(NULL), rcPoint({0,}), m_pPlayer(NULL)
 {
 
 }
@@ -14,7 +14,7 @@ MainUpdate::~MainUpdate()
 void MainUpdate::Start()
 {
 	m_hdc = GetDC(g_hWnd);
-
+	
 	m_pPlayer = new Player();
 	m_pPlayer->Start();
 }
@@ -22,15 +22,6 @@ void MainUpdate::Start()
 void MainUpdate::Update()
 {
 	m_pPlayer->Update();
-
-	/*if (m_pObj)
-		m_pObj->Update();
-
-	if (GetAsyncKeyState(VK_SPACE))
-	{
-		m_pObj = new GameObject;
-		m_pObj->Start();
-	}*/
 }
 
 void MainUpdate::Render()
@@ -38,19 +29,10 @@ void MainUpdate::Render()
 	Rectangle(m_hdc, 0, 0, 1420, 720);
 
 	m_pPlayer->Render(m_hdc);
-
-	/*if (m_pObj)
-		m_pObj->Render(m_hdc);*/
 }
 
 void MainUpdate::Destroy()
 {
 	delete m_pPlayer;
 	m_pPlayer = NULL;
-	
-	/*if (m_pObj)
-	{
-		delete m_pObj;
-		m_pObj = NULL;
-	}*/
 }
