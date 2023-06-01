50fa6e74d875f430098ef8ef92c9a489e2f43df3
zune710 <zune710@gmail.com>
2023-06-01 11:35:47 +0900

    0601 [Picking] Cursor

--- Picking/ConsolePicking/Main.cpp
+++ Picking/ConsolePicking/Main.cpp
@@ -1,12 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS  // _itoa 함수 에러 안 뜨게 하려고 사용(맨 위에 써야 함)
 #include <iostream>
 #include <Windows.h>
+#include <vector>
+#include <list>
 
 using namespace std;
 
 // ** 타일 가로, 세로 최대 개수
-#define COUNT_X 7
-#define COUNT_Y 4
+#define COUNT_X 5
+#define COUNT_Y 5
+
 
 // ** 텍스트 색
 #define BLACK		0
@@ -27,7 +30,12 @@ using namespace std;
 #define WHITE		15
 
 
-typedef struct tagVector3  // tag: 동일한 이름으로 생성되는 걸 방지하는 목적. typedef 키워드가 있음을 의미
+void SetCursorPosition(const float& _x, const float& _y);
+void SetColor(const int& _color);
+void Text(const float& _x, const float& _y, const string& _str, int _color);
+bool CheckTileList(int _index);
+
+typedef struct tagVector3
 {
 	float x, y;
 
@@ -35,29 +43,110 @@ typedef struct tagVector3  // tag: 동일한 이름으로 생성되는 걸 방지하는 목적. ty
 	tagVector3(float _x, float _y) : x(_x), y(_y) {}
 } Vector3;
 
+typedef struct tagTile
+{
+	Vector3 position[3];
+	string tile[3];
+	int option;
+	int index;
+	int color;
 
-void SetCursorPosition(const float& _x, const float& _y);
-void SetColor(int _color);
-void Text(const float& _x, const float& _y, const string& _str);
+	void Render(int _color = 15)
+	{
+		for (int i = 0; i < 3; ++i)
+			Text(position[i].x, position[i].y, tile[i], _color);
+
+		char* buffer = new char[4];
+
+		_itoa(index, buffer, 10);
+
+		Text(position[1].x + 2, position[1].y, string(buffer), _color);
+	}
+
+	tagTile() : option(0), index(0) {}
+} Tile;
+
+typedef struct tagInfo
+{
+	Vector3 position;
+	int option;
+
+	tagInfo() : option(0) {}
+} Info;
+
+
+// ** 타일 크기
+const Vector3 scale(6, 3);
+
+const int MAX = COUNT_X * COUNT_Y;
+
+list<Tile*> BlackTileList;
 
 
 int main(void)
 {
-	// ** 타일 위치
-	Vector3 position;
-	position.x = 3;  // 시작점 x
-	position.y = 2;  // 시작점 y
+	BlackTileList.clear();
 
-	// ** 타일 크기
-	Vector3 scale;
-	scale.x = 6;  // 문자열의 길이
-	scale.y = 3;
+	// ** 타일 위치
+	vector<Tile*> TileList;
+
+	int x = 0;
+	int y = 0;
+
+	for (int i = 0; i < COUNT_Y * COUNT_X; ++i)
+	{
+		Tile* tile = new Tile;
+
+		tile->tile[0] = "┌─┐";
+		tile->position[0] = Vector3(x * scale.x, y * scale.y);
+
+		tile->tile[1] = "│　│";
+		tile->position[1] = Vector3(x * scale.x, y * scale.y + 1);
+
+		tile->tile[2] = "└─┘";
+		tile->position[2] = Vector3(x * scale.x, y * scale.y + 2);
+
+		tile->option = 0;
+		tile->color = 15;
+		tile->index = y * COUNT_X + x;
+
+		x++;
+
+		if (x == 5)
+		{
+			x = 0;
+			y++;
+		}
+
+		TileList.push_back(tile);
+	}
+
+	// ** Target
+	Info Cursor;
+
+	Cursor.position = Vector3(15.0f, 8.0f);
+	Cursor.option = 0;
+
+	srand((unsigned int)GetTickCount64());
+	
+	for (int i = 0; i < 3; ++i)
+	{
+		int random = rand() % 25;
+
+		TileList[random]->option = 1;
+
+		BlackTileList.push_back(TileList[random]);
+	}
+
+
+	// ** 커서가 위치한 타일의 인덱스
+	int X = int(Cursor.position.x / scale.x);
+	int Y = int(Cursor.position.y / scale.y);
+
+	int index = Y * COUNT_X + X;
+
 
 	ULONGLONG time = GetTickCount64();
-
-	Vector3 pickPos;
-	pickPos.x = 0;
-	pickPos.y = 0;
 
 	while (true)
 	{
@@ -68,73 +157,55 @@ int main(void)
 			// ** 화면 청소
 			system("cls");
 
+			/*
+			if(!(MAX <= index))
+				TileList[index]->color = 12;
+			*/
+
+			/*
+			for (vector<Tile*>::iterator iter = TileList.begin(); iter != TileList.end(); ++iter)
+				(*iter)->Render();
+			*/
+
+			if (GetAsyncKeyState(VK_SPACE))
+				index = 0;
+
 			if (GetAsyncKeyState(VK_UP))
-				if (pickPos.y > 0)
-					pickPos.y -= scale.y;
+			{
+				if (0 < index && index < MAX && CheckTileList(index - COUNT_X))
+					index -= COUNT_X;
+			}
+			//TileList[index - COUNT_X]->option == 0 대신 CheckTileList 함수 사용
 
 			if (GetAsyncKeyState(VK_DOWN))
-				if (pickPos.y < (COUNT_Y - 1) * scale.y)
-					pickPos.y += scale.y;
+			{
+				if (0 <= index && index < MAX && CheckTileList(index + COUNT_X))
+					index += COUNT_X;
+			}
 
 			if (GetAsyncKeyState(VK_LEFT))
 			{
-				if (pickPos.x > 0)
-					pickPos.x -= scale.x;
-				else if (pickPos.y > 0)
-				{
-					pickPos.x = (COUNT_X - 1) * scale.x;
-					pickPos.y -= scale.y;
-				}
+				if (0 < index && index < MAX && CheckTileList(index - 1))
+					index -= 1;
 			}
 
 			if (GetAsyncKeyState(VK_RIGHT))
 			{
-				if (pickPos.x < (COUNT_X - 1) * scale.x)
-					pickPos.x += scale.x;
-				else if (pickPos.y < (COUNT_Y - 1) * scale.y)
-				{
-					pickPos.x = 0;
-					pickPos.y += scale.y;
-				}
+				if (0 <= index && index < MAX && CheckTileList(index + 1))
+					index += 1;
 			}
 
-			for (int y = 0; y < COUNT_Y; ++y)
+
+			for (int i = 0; i < TileList.size(); ++i)
 			{
-				for (int x = 0; x < COUNT_X; ++x)
+				if (index == i)
+					TileList[i]->Render(12);
+				else
 				{
-					SetColor(7);
-
-					// ** 타일 출력
-					Text(position.x - (scale.x * 0.5f) + scale.x * x,
-						position.y - (scale.y * 0.5f) + scale.y * y,
-						"┌─┐");
-
-					Text(position.x - (scale.x * 0.5f) + scale.x * x,
-						position.y - (scale.y * 0.5f) + scale.y * y + 1,
-						"│　│");
-
-					Text(position.x - (scale.x * 0.5f) + scale.x * x,
-						position.y - (scale.y * 0.5f) + scale.y * y + 2,
-						"└─┘");
-
-					// ** index 확인
-					int index = y * COUNT_X + x;
-
-					char* buffer = new char[4];
-					_itoa(index, buffer, 10);
-
-					int posX = int(position.x - (scale.x * 0.5f) + scale.x * x);
-					int posY = int(position.y - (scale.y * 0.5f) + scale.y * y);
-
-					if ((int)pickPos.x == posX && (int)pickPos.y == posY)
-						SetColor(14);
+					if (TileList[i]->option == 1)
+						TileList[i]->Render(0);
 					else
-						SetColor(12);
-
-					// index 출력
-					Text(position.x - 1 + scale.x * x,
-						position.y - (scale.y * 0.5f) + scale.y * y + 1,
-						string(buffer));
+						TileList[i]->Render(15);
 				}
 			}
 
@@ -142,7 +213,7 @@ int main(void)
 			Sleep(50);
 		}
 	}
-	
+
 	SetColor(7);
 
 	return 0;
@@ -150,21 +221,32 @@ int main(void)
 
 void SetCursorPosition(const float& _x, const float& _y)
 {
-	COORD pos = { _x, _y };
+	COORD pos = { (SHORT)_x, (SHORT)_y };
 
 	SetConsoleCursorPosition(
 		GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
 
-void SetColor(int _color)
+void SetColor(const int& _color)
 {
 	HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
 
 	SetConsoleTextAttribute(handle, _color);
 }
 
-void Text(const float& _x, const float& _y, const string& _str)
+void Text(const float& _x, const float& _y, const string& _str, int _color = 15)
 {
+	SetColor(_color);
 	SetCursorPosition(_x, _y);
 	cout << _str << endl;
+}
+
+bool CheckTileList(int _index)
+{
+	for (list<Tile*>::iterator iter = BlackTileList.begin(); iter != BlackTileList.end(); ++iter)
+	{
+		if ((*iter)->index == _index || _index < 0 || _index >= MAX)
+			return false;
+	}
+	return true;
 }
